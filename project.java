ec81505e26c1a1afb451bcb5c166715f8c3f1768
barneder2025 <164350924+barneder2025@users.noreply.github.com>
2024-04-18 10:00:56 -0400

    Update project.java

--- project.java
+++ project.java
@@ -1,5 +1,5 @@
 public class project {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         /*  Classes
         Spacecraft - fuel/inventory - Rocket editor
             |
@@ -13,5 +13,21 @@ public class project {
 
         Save/load function
         */
+
+       /*  rocket calculations basics/guide
+        countdown reach 0 launch for set # of sec based on fuel amount 
+
+        1lb fuel in 1 sec increase speed by 30m per sec 
+        1lb in 2 sec increse by 15m per sec
+        2lb in 2 sec increase by 60m per sec
+
+        out of fuel falling 
+        astronauts EVA for 5 minutes
+        speed reduce by 9.81m per sec 
+        then gain speed 9.81m per sec until reach atmosphere
+        burn if faster than 3,000m per sec in atmosphere
+        atmosphere ends at 70,000m
+        below 10,000m parachutes reduce speed no faster than 7m per sec
+       */
     }
 }
