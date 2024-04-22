82fd5c20936a5d181cf128c94d8b143f286d0aa3
barneder2025 <164350924+barneder2025@users.noreply.github.com>
2024-04-22 09:35:11 -0400

    launch made

---
+++ launch.java
@@ -0,0 +1,22 @@
+public class launch {
+
+    /* rocket calculations basics/guide
+        countdown reach 0 launch for set # of sec based on fuel amount 
+
+        1lb fuel in 1 sec increase speed by 30m per sec 
+        1lb in 2 sec increse by 15m per sec
+        2lb in 2 sec increase by 60m per sec
+
+        out of fuel falling 
+        astronauts EVA/EAV for 5 minutes
+        speed reduce by 9.81m per sec 
+        then gain speed 9.81m per sec until reach atmosphere
+        burn if faster than 3,000m per sec in atmosphere
+        atmosphere ends at 70,000m
+        below 10,000m parachutes reduce speed no faster than 7m per sec
+        */
+
+
+
+
+}
