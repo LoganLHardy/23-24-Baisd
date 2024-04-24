291674fede465c6fd640929f4b2e91e7202adb73
barneder2025 <164350924+barneder2025@users.noreply.github.com>
2024-04-24 08:54:36 -0400

    .

--- astronaut.java
+++ astronaut.java
@@ -1,3 +1,14 @@
 public class Astronaut {
 
+    String name;
+    int birthdate;
+    int serial;
+    String address;
+    String email;
+    int phone;
+    double payrate;
+    double weight;
+    String nextkin;
+    String status;
+
 }
--- project.java
+++ project.java
@@ -1,19 +1,8 @@
 public class Project {
     public static void main (String[] args) {
-        /*  Classes
-        Spacecraft - fuel/inventory - Rocket editor
-            |
-        Astronauts - astronaut editor - secure astronaut info
-                                            |
-                    password database - password verifier - admin password
-                                            |
-                                        Interface/gui
-                                            |
-            Fuel calculations - Launch - ascent/descent - landing/crash
 
-        Save/load function
-        */
-
+    String[] astronauts;
+    String[] spacecraft;
         
     }
 }
